9f5bb84989277bb67ee428ec631e818595f25899
ductran <>
2021-06-21 15:19:28 +0700

    HTML EMBEDED

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -32,6 +32,7 @@ import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformatio
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'; 
 import MathType from '@wiris/mathtype-ckeditor5';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed';
 
 
 export default class ClassicEditor extends ClassicEditorBase {}
@@ -64,6 +65,7 @@ ClassicEditor.builtinPlugins = [
 	TextTransformation,
 	Alignment,
 	MathType,
+	HtmlEmbed
 ];
 
 // Editor configuration.
@@ -72,6 +74,7 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'ChemType',
 			'MathType',
+			'htmlEmbed',
 			'heading',
 			'|',
 			'bold',
